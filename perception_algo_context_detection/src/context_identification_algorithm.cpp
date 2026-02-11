980c0538076ee6bc0c0655768b8867cf598887a8
Kalana Ratnayake <kalanaratnayake95@gmail.com>
2026-02-11 06:56:01 +0000

    moved experimental packages to develop branch

--- perception_algo_context_detection/src/context_identification_algorithm.cpp
+++
@@ -1,5 +0,0 @@
-#include <pluginlib/class_list_macros.hpp>
-#include <perception_base/algorithm_base.hpp>
-#include <perception_algo_context_detection/ContextAlgorithm.hpp>
-
-PLUGINLIB_EXPORT_CLASS(perception::ContextAlgorithm, perception::AlgorithmBase);
